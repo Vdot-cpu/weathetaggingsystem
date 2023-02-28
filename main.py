e22b61994a974572cd2b7c66500332922792714e
Mathure Uththerakunaseelan <mathure.u@gmail.com>
2023-02-28 23:18:53 +0000

    Tried to resolve syntax error

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+#!/usr/local/bin/python
+# -*- coding: iso-8859-1 -*-
+
 import requests
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -49,7 +52,6 @@ def set_end_date_time(year, month, day):
 # Set date range
 set_start_date_time(2023, 2, 17)
 set_end_date_time(2023, 2, 24)
-
 
 # granularity "1m" "2m" "5m" "15m" "30m" "60m" "90m" "1h" “1d”, “5d”, “1mo”, “3mo”, “6mo”, “1y”, “2y”, “5y”, “10y”, “ytd”, “max”
 
@@ -106,7 +108,3 @@ def plot_data(ticker, interval, location):
 set_start_date_time(2023, 2, 20)
 set_end_date_time(2023, 2, 25)
 plot_data(consumerGood_ticker, '1d', london)
-
-
-
-
